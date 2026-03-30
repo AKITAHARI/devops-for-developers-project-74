9df4c09748b594f40f19339f7abcae3ea0714dda
AKITAHARI <akitahari.09@gmail.com>
2026-03-30 17:03:53 +0300

    Fix PostgreSQL connection for tests

--- app/config/config.cjs
+++ app/config/config.cjs
@@ -15,10 +15,10 @@ module.exports = {
   },
   test: {
     dialect: 'postgres',
-    database: process.env.DATABASE_NAME,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    port: process.env.DATABASE_PORT,
-    host: process.env.DATABASE_HOST,
+    database: process.env.DATABASE_NAME || 'postgres',
+    username: process.env.DATABASE_USERNAME || 'postgres',
+    password: process.env.DATABASE_PASSWORD || 'password',
+    host: process.env.DATABASE_HOST || 'db',
+    port: process.env.DATABASE_PORT || 5432,
   },
 };
